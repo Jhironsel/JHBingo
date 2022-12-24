1e961ea5e964e5bf197cb9baee54283b811ac1db
jhironsel <jhironseld@gmail.com>
2022-12-23 22:01:41 -0400

    Aqui he practicado con servlet y parametros, en la clase servicioWeb contiene el metodo doPost el cual aplico como manejar parametros.

--- src/main/java/sur/softsurena/JHBingo/JakartaRestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package sur.softsurena.JHBingo;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JakartaRestConfiguration extends Application {
-    
-}
--- src/main/java/web/servicioWeb.java
+++ src/main/java/web/servicioWeb.java
@@ -8,25 +8,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/servicio")
-public class servicio extends HttpServlet {
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        try (PrintWriter out = resp.getWriter();) {
-            out.println("Saludos desde doGet");
-        }
-
-    }
+@WebServlet("/servicioWeb")
+public class servicioWeb extends HttpServlet{
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        try (PrintWriter out = resp.getWriter();) {
-            out.println("Saludos desde doPost");
+        //super.doPost(req, resp);
+        String usuario = req.getParameter("txtUsuario");
+        String clave = req.getParameter("txtClave");
+        
+        
+        try(PrintWriter out = resp.getWriter();){
+            out.print("El usuario enviado es: "+usuario);
+            out.print("<br>");
+            out.print("La clave enviada es: "+clave);
         }
-
     }
-
+    
 }
